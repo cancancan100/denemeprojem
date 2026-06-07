28c7f25d86fba85a7a04ca321ef11ab763abba09
cancancan100 <61051302+cancancan100@users.noreply.github.com>
2026-06-08 02:11:43 +0300

    Revert "can"

--- main.py
+++ main.py
@@ -3,5 +3,5 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-sasas
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
